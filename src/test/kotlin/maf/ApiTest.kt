9a7d86d16c7e77d6c65965d94642888b4cc2668c
Marco Fracassi <fracassi.marco@gmail.com>
2018-06-05 14:17:04 +0200

    Added homepage api test (ignored)

---
+++ src/test/kotlin/maf/ApiTest.kt
@@ -0,0 +1,30 @@
+package maf
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.exchange
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Ignore
+class ApiTest {
+
+    @Autowired
+    protected lateinit var restTemplate: TestRestTemplate
+
+    @Test
+    fun shouldRenderHomePage() {
+        val response = restTemplate.exchange<String>("/", HttpMethod.GET)
+
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.body).contains("form")
+    }
+}
