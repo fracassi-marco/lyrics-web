61f71531103645b2dd8bb62c5c61ed0ff950c169
Marco Fracassi <fracassi.marco@gmail.com>
2018-07-04 12:58:18 +0200

    Added acceptance test with WireMock

--- src/test/kotlin/maf/LocalWireMockAcceptanceTest.kt
+++ src/test/kotlin/maf/LocalWireMockAcceptanceTest.kt
@@ -1,5 +1,6 @@
 package maf
 
+import com.github.tomakehurst.wiremock.client.WireMock.*
 import org.assertj.core.api.Assumptions.assumeThat
 import org.junit.After
 import org.junit.Before
@@ -9,37 +10,41 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.junit4.SpringRunner
+import com.github.tomakehurst.wiremock.junit.WireMockRule
+import org.junit.Rule
+
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class LocalAcceptanceTest {
+class LocalWireMockAcceptanceTest {
 
     @LocalServerPort
     private var _port: Int = 0
     private lateinit var _browser: PhantomJSDriver
-    private lateinit var _lyricsOvh: LyricsOvh
-    private lateinit var _lyricsOvhServer: ApplicationServer
     private lateinit var _homePage: HomePage
+
+    @get:Rule
+    val wireMockRule = WireMockRule(9091)
 
     @Before
     fun before() {
         System.setProperty(LYRICSOVH_ENDPOINT_PROPERTY, "http://localhost:9091")
         _browser = PhantomJSDriver()
         _homePage = HomePage(_browser, _port)
-        _lyricsOvh = LyricsOvh()
-        _lyricsOvhServer = ApplicationServer(9091, _lyricsOvh).start()
     }
 
     @After
     fun after() {
         _browser.close()
-        _lyricsOvhServer.close()
         System.clearProperty(LYRICSOVH_ENDPOINT_PROPERTY)
     }
 
     @Test
     fun shouldFindALyrics() {
-        _lyricsOvh.calledWith("Aqua", "Barbie Girl").thanReturn("You can brush my hair")
+        stubFor(get("/v1/Aqua/Barbie%20Girl")
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"lyrics\":\"You can brush my hair\"}")));
 
         _homePage.open()
         _homePage.searchFor("Aqua", "Barbie Girl")
