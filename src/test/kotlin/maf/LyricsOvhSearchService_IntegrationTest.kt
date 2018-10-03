d18da47966b1d9946661bcc03c1d6dff58633ad7
Marco Fracassi <fracassi.marco@gmail.com>
2018-10-03 14:23:27 +0200

    Added integration test on lyrics not found

--- src/test/kotlin/maf/LyricsOvhSearchService_IntegrationTest.kt
+++ src/test/kotlin/maf/LyricsOvhSearchService_IntegrationTest.kt
@@ -1,9 +1,9 @@
 package maf
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.After
-import org.junit.Before
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.Test
+import org.springframework.web.client.HttpClientErrorException
 
 class LyricsOvhSearchService_IntegrationTest {
 
@@ -12,5 +12,12 @@ class LyricsOvhSearchService_IntegrationTest {
         val result = LyricsOvhSearchService(Beans().httpClient()).search("U2", "Pride")
 
         assertThat(result).contains("in the name of love")
+    }
+
+    @Test
+    fun shouldHandleLyricsNotFound() {
+        assertThatThrownBy { LyricsOvhSearchService(Beans().httpClient()).search("U2", "Not existing") }
+                .isInstanceOf(HttpClientErrorException::class.java)
+                .hasMessageContaining("404 Not Found")
     }
 }
